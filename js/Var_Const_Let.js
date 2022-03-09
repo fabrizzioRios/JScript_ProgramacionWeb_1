bf2bbbf23bd0d08fab0b8858b1400d88e7d29858
fabrizzioRios <warmachine860gmail.com>
2022-03-09 09:13:47 -0600

    3er Commit de Javascript en Programacion Web 1

---
+++ js/Var_Const_Let.js
@@ -0,0 +1,37 @@
+var edad = 10
+let arrayNumero = [{
+    "name": "luis1",
+    "edad": "16"
+},{
+    "name": "luis2",
+    "edad": "17"
+}, {
+    "name": "luis3",
+    "edad": "18"
+}]
+
+let person = {
+    "name": "luis",
+    "edad": 16
+}
+
+for (index = 0; index < arrayNumero.length; index++) {
+    console.log(arrayNumero[index]);
+}
+
+for (let variable in person) {
+    console.log(variable + " is " + person[variable])
+}
+
+if (true) {
+    let arrayNumero = [10, 20, 30, 40]
+    var edad = 20
+    console.log(edad)
+    console.log(arrayNumero)
+}
+console.log(edad)
+
+for (let hola in arrayNumero) {
+    console.log((arrayNumero[hola].name))
+}
+
